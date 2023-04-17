23e94bdb867b421847b02019331f2ae07cfb6959
Fedorenkow <oleh.yushchuk@gmail.com>
2023-04-17 18:12:32 +0300

    Create it_college_type.py

---
+++ api/models/it_college_type.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+class it_college_type(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    registered = db.Column(db.DateTime, default=datetime.utcnow)
