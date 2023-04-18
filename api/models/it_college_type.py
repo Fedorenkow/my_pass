acbbca2d699e9d2afd6d5832969da8f320e617e0
Fedorenkow <oleh.yushchuk@gmail.com>
2023-04-18 18:00:34 +0300

    Update it_college_type.py

--- api/models/it_college_type.py
+++ api/models/it_college_type.py
@@ -10,3 +10,6 @@ class it_college_type(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(30), nullable=False)
     registered = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<it_college:{self.id}>"
