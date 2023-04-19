0fc22460c37ebc9ddcfc661355a152d80515f5cd
Fedorenkow <oleh.yushchuk@gmail.com>
2023-04-19 17:16:07 +0300

    Track modification =>False

--- api/__init__.py
+++ api/__init__.py
@@ -6,6 +6,6 @@ db = SQLAlchemy()
 
 
 def create_app():
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1310526879@localhost:5432/mypass_test'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1310526879@localhost:5432/mypass'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     return app
