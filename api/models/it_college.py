ee6a4b26f6c0571bea7664b42564a321ddd2c6ca
Fedorenkow <oleh.yushchuk@gmail.com>
2023-04-19 16:25:39 +0300

    Update it_college.py

--- api/models/it_college.py
+++ api/models/it_college.py
@@ -1,9 +1,5 @@
 from datetime import datetime
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
+from api import db
 
 
 class it_college(db.Model):
