a9ddc5db69def746a1a7d4a6bca0b245b22dce87
Fedorenkow <oleh.yushchuk@gmail.com>
2023-05-02 18:06:40 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 api = Api(app)
 
 with app.app_context():
