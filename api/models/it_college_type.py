49148fe638d350490a9c90ee1e07f703718f0861
Fedorenkow <oleh.yushchuk@gmail.com>
2023-05-10 19:07:35 +0300

    Update it_college_type.py

--- api/models/it_college_type.py
+++ api/models/it_college_type.py
@@ -3,7 +3,7 @@ from api.app import db
 
 
 class it_college_type(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
     name = db.Column(db.String(30), nullable=False)
     registered = db.Column(db.DateTime, default=datetime.utcnow)
 
