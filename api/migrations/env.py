406031d93447f6db829b44722b446e79dcda7981
Fedorenkow <oleh.yushchuk@gmail.com>
2023-05-17 02:29:56 +0300

    Update env.py

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -36,6 +36,7 @@ def get_engine_url():
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
 from api.models.it_college import it_college
 from api.models.it_college_type import it_college_type
 
