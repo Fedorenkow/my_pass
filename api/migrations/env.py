343b613645cfd79fbadb3a9a973100ad1316c122
Fedorenkow <oleh.yushchuk@gmail.com>
2023-05-17 01:21:35 +0300

    Update env.py

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -44,7 +44,6 @@ target_metadata = [it_college.metadata, it_college_type.metadata]
 config.set_main_option('sqlalchemy.url', get_engine_url())
 target_db = current_app.extensions['migrate'].db
 
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
