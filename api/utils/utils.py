b011333c4dbcf407cd82e3b936e5c4aa2aa47572
Fedorenkow <oleh.yushchuk@gmail.com>
2023-05-27 21:35:13 +0300

    Update utils.py

--- api/utils/utils.py
+++ api/utils/utils.py
@@ -1,5 +1,15 @@
 import random
 
+from api.models.it_college import it_college
+
 
 def generate_unique_code():
     return random.randint(1000000000, 9999999999)
+
+
+def check_unique_code(code):
+    check = it_college.query.filter_by(unique_code=str(code)).first()
+    if check:
+        return True
+    else:
+        return False
